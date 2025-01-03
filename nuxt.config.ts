7ff4fc21d90038da7082875833b5f9e74287a098
dhikrama <dhikrama.creator@gmail.com>
2025-01-03 19:31:55 +0700

    Penambahan whatsapp button

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -38,7 +38,7 @@ export default defineNuxtConfig({
 
     // plugins: ['~/plugins/canonical.js'],
 
-    modules: ['@nuxt/image'],
+    modules: ['@nuxt/image', '@nuxt/ui'],
     image: {
       domains: ['cdn.jsdelivr.net'], // Daftarkan domain gambar yang diizinkan
       provider: 'ipx', // Gunakan provider bawaan
